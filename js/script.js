c0bf1e45c991652dcba28d92c303b9fdffa9b695
eugenio.malavasi <eugenio.malavasi@gmail.com>
2024-04-20 09:27:26 +0200

    now the enter key actually works

--- js/script.js
+++ js/script.js
@@ -24,21 +24,23 @@ createApp({
         }
     }, methods: {
         addToDo: function () {
-            if( this.newTodo.text !== "") {
-            const newTodoCopy = { ...this.newTodo }
-            this.toDo.push(newTodoCopy);
-            this.newTodo.text = "";
-            
+            if (this.newTodo.text !== "") {
+                const newTodoCopy = { ...this.newTodo }
+                this.toDo.push(newTodoCopy);
+                this.newTodo.text = "";
             } else {
-                alert("inserisci un task")
-
+                alert("inserisci un task");
             }
         }, delFunc: function (index) {
-            this.toDo.splice(index,1)
-        }, iconChange: function (index) {
-            if(this.toDo[index].done) {
+            this.toDo.splice(index, 1)
+        }, iconChange: function (curObj) {
+            if (curObj.done) {
                 this.icon = this.redIcon;
+            } else {
+                this.icon = "fa-solid fa-square-check";
             }
+        }, check: function () {
+            console.log("enter");
         }
     }
 }).mount('#app')
